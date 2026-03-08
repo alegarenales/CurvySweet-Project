f2f9951c877a37aa941e57b1602fea0a465770f7
alegarenales <alegarenales@gmail.com>
2026-03-08 22:07:45 +0100

    add cloudfrade adapter

--- zapping-zero/astro.config.mjs
+++ zapping-zero/astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: node({
-        mode: 'standalone',
-    }),
+    adapter:
+        // mode: 'standalone',
+        cloudflare(),
 });
